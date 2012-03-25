0c8587d300ee3a5dd031eeb556f50c43ea10245a
Punit Rathore <punitr@gmail.com>
2012-03-25 11:45:27 -0700

    adding chapter 7! setsgts

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject my-little-clojurer "1.0.0-SNAPSHOT"
   :description "The Little Schemer in Clojure"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [lein-swank "1.4.4"]])
