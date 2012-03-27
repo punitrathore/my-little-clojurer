8411e9f9cde77af01b19e7cd56a1272b397879d9
Punit Rathore <punitr@gmail.com>
2012-03-27 09:37:13 -0700

    finishing chapter 8. Phew!

--- src/my_little_clojurer/chapter8.clj
+++ src/my_little_clojurer/chapter8.clj
@@ -144,3 +144,99 @@
 
 (defn multirember1 [lat]
   ((multiremberT (fn [x] (= 1 x))) lat))
+
+
+(defn multirember&co [a lat col]
+  (cond (empty? lat)
+        (col '() '())
+
+        (= (first lat) a)
+        (multirember&co a (rest lat)
+                        (fn [newlat seen]
+                          (col newlat
+                                (cons (first lat) seen))))
+
+        :else
+        (multirember&co a (rest lat)
+                        (fn [newlat seen]
+                          (col (cons (first lat) newlat)
+                                seen)))))
+
+(defn a-friend? [x y]
+  (empty? y))
+
+(defn multiinsertLR [new oldL oldR lat]
+  (cond (empty? lat)
+        '()
+
+        (= (first lat) oldL)
+        (cons new (cons oldL (multiinsertLR new oldL oldR (rest lat))))
+
+        (= (first lat) oldR)
+        (cons oldR (cons new (multiinsertLR new oldL oldR (rest lat))))
+              
+        :else
+        (cons (first lat) (multiinsertLR new oldL oldR (rest lat)))))
+
+
+(defn multiinsertLR&co [new oldL oldR lat col]
+  (cond (empty? lat)
+        (col '() 0 0)
+
+        (= (first lat) oldL)
+        (multiinsertLR&co new oldL oldR (rest lat)
+                          (fn [newlat L R]
+                            (col (cons new (cons oldL) newlat) (add1 L) R)))
+
+        (= (first lat) oldR)
+        (multiinsertLR&co new oldL oldR (rest lat)
+                          (fn [newlat L R]
+                            (col (cons oldR (cons new newlat)) L (add1 R))))
+
+        :else
+        (multirember&co new oldL oldR (rest lat)
+                        (fn [newlat L R]
+                          (col (cons (first lat) newlat) L R)))))
+
+(defn evens-only* [l]
+  (cond (empty? l)
+        '()
+
+        (atom? (first l))
+        (cond (even? (first l))
+              (cons (first l) (evens-only* (rest l)))
+
+              :else
+              (evens-only* (rest l)))
+
+        :else
+        (cons (evens-only* (first l)) (evens-only* (rest l)))))
+
+(defn evens-only*&co [l col]
+  (cond (empty? l)
+        (col '() 1 0)
+
+        (atom? (first l))
+        (cond (even? (first l))
+              (evens-only*&co (rest l)
+                              (fn [newlat prod sum]
+                                (col (cons (first l) newlat) (* prod (first l)) sum)))
+
+              :else
+              (evens-only*&co (rest l)
+                              (fn [newlat prod sum]
+                                (col newlat prod (+ sum (first l))))))
+
+        :else
+        (evens-only*&co (first l)
+                        (fn [al ap as]
+                          (evens-only*&co (rest l)
+                                          (fn [dl dp ds]
+                                            (col (cons (first al) dl)
+                                                 (* ap dp)
+                                                 (+ as ds))))))))
+
+(defn the-last-friend [newl product sum]
+  (cons sum
+        (cons product
+              newl)))
